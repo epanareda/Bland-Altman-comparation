d44cfb9888bdbf08f84e197c6b6c07560ce8ef40
epanareda <73385694+epanareda@users.noreply.github.com>
2021-12-17 22:19:49 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,860 @@
+const T_TABLE_CI = {
+  1: {
+    "50%": 1.000,
+    "60%": 1.376,
+    "70%": 1.963,
+    "80%": 3.078,
+    "90%": 6.314,
+    "95%": 12.71,
+    "98%":  31.82,
+    "99%": 63.66,
+    "99.8%": 318.31,
+    "99.9%": 636.62
+  },
+  2: {
+    "50%": 0.816,
+    "60%": 1.061,
+    "70%": 1.386,
+    "80%": 1.886,
+    "90%": 2.920,
+    "95%": 4.303,
+    "98%": 6.965,
+    "99%": 9.925,
+    "99.8%": 22.327,
+    "99.9%": 31.599
+  },
+  3: {
+    "50%": 0.765,
+    "60%": 0.978,
+    "70%": 1.250,
+    "80%": 1.638,
+    "90%": 2.353,
+    "95%": 3.182,
+    "98%": 4.541,
+    "99%": 5.841,
+    "99.8%": 10.215,
+    "99.9%": 12.924
+  },
+  4: {
+    "50%": 0.741,
+    "60%": 0.941,
+    "70%": 1.190,
+    "80%": 1.533,
+    "90%": 2.132,
+    "95%": 2.776,
+    "98%": 3.747,
+    "99%": 4.604,
+    "99.8%": 7.173,
+    "99.9%": 8.610
+  },
+  5: {
+    "50%": 0.727,
+    "60%": 0.920,
+    "70%": 1.156,
+    "80%": 1.476,
+    "90%": 2.015,
+    "95%": 2.571,
+    "98%": 3.365,
+    "99%": 4.032,
+    "99.8%": 5.893,
+    "99.9%": 6.869
+  },
+  6: {
+    "50%": 0.718,
+    "60%": 0.906,
+    "70%": 1.134,
+    "80%": 1.440,
+    "90%": 1.943,
+    "95%": 2.447,
+    "98%": 3.143,
+    "99%": 3.707,
+    "99.8%": 5.208,
+    "99.9%": 5.959
+  },
+  7: {
+    "50%": 0.711, 
+    "60%": 0.896,
+    "70%": 1.119,
+    "80%": 1.415,
+    "90%": 1.895,
+    "95%": 2.365,
+    "98%": 2.998,
+    "99%": 3.499,
+    "99.8%": 4.785,
+    "99.9%": 5.408
+  },
+  8: {
+    "50%": 0.706,
+    "60%": 0.889,
+    "70%": 1.108,
+    "80%": 1.397,
+    "90%": 1.860,
+    "95%": 2.306,
+    "98%": 2.896,
+    "99%": 3.355,
+    "99.8%": 4.501,
+    "99.9%": 5.041
+  },
+  9: {
+    "50%": 0.703,
+    "60%": 0.883, 
+    "70%": 1.100,
+    "80%": 1.383,
+    "90%": 1.833,
+    "95%": 2.262,
+    "98%": 2.821,
+    "99%": 3.250,
+    "99.8%": 4.297,
+    "99.9%": 4.781
+  },
+  10: {
+    "50%": 0.700, 
+    "60%": 0.879,
+    "70%": 1.093,
+    "80%": 1.372,
+    "90%": 1.812,
+    "95%": 2.228,
+    "98%": 2.764,
+    "99%": 3.169,
+    "99.8%": 4.144,
+    "99.9%": 4.587
+  },
+  11: {
+    "50%": 0.697,
+    "60%": 0.876,
+    "70%": 1.088,
+    "80%": 1.363,
+    "90%": 1.796,
+    "95%": 2.201,
+    "98%": 2.718,
+    "99%": 3.106,
+    "99.8%": 4.025,
+    "99.9%": 4.437
+  },
+  12: {
+    "50%": 0.695,
+    "60%": 0.873,
+    "70%": 1.083,
+    "80%": 1.356,
+    "90%": 1.782,
+    "95%": 2.179,
+    "98%": 2.681,
+    "99%": 3.055,
+    "99.8%": 3.930,
+    "99.9%": 4.318
+  },
+  13: {
+    "50%": 0.694,
+    "60%": 0.870,
+    "70%": 1.079,
+    "80%": 1.350,
+    "90%": 1.771,
+    "95%": 2.160,
+    "98%": 2.650,
+    "99%": 3.012,
+    "99.8%": 3.852,
+    "99.9%": 4.221
+  },
+  14: {
+    "50%": 0.692, 
+    "60%": 0.868,
+    "70%": 1.076,
+    "80%": 1.345,
+    "90%": 1.761,
+    "95%": 2.145,
+    "98%": 2.624,
+    "99%": 2.977,
+    "99.8%": 3.787,
+    "99.9%": 4.140
+  },
+  15: {
+    "50%": 0.691, 
+    "60%": 0.866,
+    "70%": 1.074,
+    "80%": 1.341,
+    "90%": 1.753,
+    "95%": 2.131,
+    "98%": 2.602,
+    "99%": 2.947,
+    "99.8%": 3.733,
+    "99.9%": 4.073
+  },
+  16: {
+    "50%": 0.690,
+    "60%": 0.865,
+    "70%": 1.071,
+    "80%": 1.337,
+    "90%": 1.746,
+    "95%": 2.120,
+    "98%": 2.583,
+    "99%": 2.921,
+    "99.8%": 3.686,
+    "99.9%": 4.015
+  },
+  17: {
+    "50%": 0.689,
+    "60%": 0.863,
+    "70%": 1.069,
+    "80%": 1.333,
+    "90%": 1.740,
+    "95%": 2.110,
+    "98%": 2.567,
+    "99%": 2.898,
+    "99.8%": 3.646,
+    "99.9%": 3.965
+  },
+  18: {
+    "50%": 0.688,
+    "60%": 0.862,
+    "70%": 1.067,
+    "80%": 1.330,
+    "90%": 1.734,
+    "95%": 2.101,
+    "98%": 2.552,
+    "99%": 2.878,
+    "99.8%": 3.610,
+    "99.9%": 3.922
+  },
+  19: {
+    "50%": 0.688,
+    "60%": 0.861,
+    "70%": 1.066,
+    "80%": 1.328,
+    "90%": 1.729,
+    "95%": 2.093,
+    "98%": 2.539,
+    "99%": 2.861,
+    "99.8%": 3.579,
+    "99.9%": 3.883
+  },
+  20: {
+    "50%": 0.687,
+    "60%": 0.860,
+    "70%": 1.064,
+    "80%": 1.325,
+    "90%": 1.725,
+    "95%": 2.086,
+    "98%": 2.528,
+    "99%": 2.845,
+    "99.8%": 3.552,
+    "99.9%": 3.850
+  },
+  21: {
+    "50%": 0.686,
+    "60%": 0.859,
+    "70%": 1.063,
+    "80%": 1.323,
+    "90%": 1.721,
+    "95%": 2.080,
+    "98%": 2.518,
+    "99%": 2.831,
+    "99.8%": 3.527,
+    "99.9%": 3.819
+  },
+  22: {
+    "50%": 0.686,
+    "60%": 0.858,
+    "70%": 1.061,
+    "80%": 1.321,
+    "90%": 1.717,
+    "95%": 2.074,
+    "98%": 2.508,
+    "99%": 2.819,
+    "99.8%": 3.505,
+    "99.9%": 3.792
+  },
+  23: {
+    "50%": 0.685,
+    "60%": 0.858,
+    "70%": 1.060,
+    "80%": 1.319,
+    "90%": 1.714,
+    "95%": 2.069,
+    "98%": 2.500,
+    "99%": 2.807,
+    "99.8%": 3.485,
+    "99.9%": 3.768
+  },
+  24: {
+    "50%": 0.685,
+    "60%": 0.857,
+    "70%": 1.059,
+    "80%": 1.318,
+    "90%": 1.711,
+    "95%": 2.064,
+    "98%": 2.492,
+    "99%": 2.797,
+    "99.8%": 3.467,
+    "99.9%": 3.745
+  },
+  25: {
+    "50%": 0.684,
+    "60%": 0.856,
+    "70%": 1.058,
+    "80%": 1.316,
+    "80%": 1.708,
+    "95%": 2.060,
+    "98%": 2.485,
+    "99%": 2.787,
+    "99.8%": 3.450,
+    "99.9%": 3.725
+  },
+  26: {
+    "50%": 0.684,
+    "60%": 0.856,
+    "70%": 1.058,
+    "80%": 1.315,
+    "90%": 1.706,
+    "95%": 2.056,
+    "98%": 2.479,
+    "99%": 2.779,
+    "99.8%": 3.435,
+    "99.9%": 3.707
+  },
+  27: {
+    "50%": 0.684,
+    "60%": 0.855,
+    "70%": 1.057,
+    "80%": 1.314,
+    "90%": 1.703,
+    "95%": 2.052,
+    "98%": 2.473,
+    "99%": 2.771,
+    "99.8%": 3.421,
+    "99.9%": 3.690
+  },
+  28: {
+    "50%": 0.683,
+    "60%": 0.855,
+    "70%": 1.056,
+    "80%": 1.313,
+    "90%": 1.701,
+    "95%": 2.048,
+    "98%": 2.467,
+    "99%": 2.763,
+    "99.8%": 3.408,
+    "99.9%": 3.674
+  },
+  29: {
+    "50%": 0.683,
+    "60%": 0.854,
+    "70%": 1.055,
+    "80%": 1.311,
+    "90%": 1.699,
+    "95%": 2.045,
+    "98%": 2.462,
+    "99%": 2.756,
+    "99.8%": 3.396,
+    "99.9%": 3.659
+  },
+  30: {
+    "50%": 0.683,
+    "60%": 0.854,
+    "70%": 1.055,
+    "80%": 1.310,
+    "90%": 1.697,
+    "95%": 2.042,
+    "98%": 2.457,
+    "99%": 2.750,
+    "99.8%": 3.385,
+    "99.9%": 3.646
+  },
+  40: {
+    "50%": 0.681,
+    "60%": 0.851,
+    "70%": 1.050,
+    "80%": 1.303,
+    "90%": 1.684,
+    "95%": 2.021,
+    "98%": 2.423,
+    "99%": 2.704,
+    "99.8%": 3.307,
+    "99.9%": 3.551
+  },
+  60: {
+    "50%": 0.679,
+    "60%": 0.848,
+    "70%": 1.045,
+    "80%": 1.296,
+    "90%": 1.671,
+    "95%": 2.000,
+    "98%": 2.390,
+    "99%": 2.660,
+    "99.8%": 3.232,
+    "99.9%": 3.460
+  },
+  80: {
+    "50%": 0.678,
+    "60%": 0.846,
+    "70%": 1.043,
+    "80%": 1.292,
+    "90%": 1.664,
+    "95%": 1.990,
+    "98%": 2.374,
+    "99%": 2.639,
+    "99.8%": 3.195,
+    "99.9%": 3.416
+  },
+  100: {
+    "50%": 0.677,
+    "60%": 0.845,
+    "70%": 1.042,
+    "80%": 1.290,
+    "90%": 1.660,
+    "95%": 1.984,
+    "98%": 2.364,
+    "99%": 2.626,
+    "99.8%": 3.174,
+    "99.9%": 3.390
+  },
+  1000: {
+    "50%": 0.675,
+    "60%": 0.842,
+    "70%": 1.037,
+    "80%": 1.282,
+    "90%": 1.646,
+    "95%": 1.962,
+    "98%": 2.330,
+    "99%": 2.581,
+    "99.8%": 3.098,
+    "99.9%": 3.300
+  },
+  "z": {
+    "50%": 0.674,
+    "60%": 0.842,
+    "70%": 1.036,
+    "80%": 1.282,
+    "90%": 1.645,
+    "95%": 1.960,
+    "98%": 2.326,
+    "99%": 2.576,
+    "99.8%": 3.090,
+    "99.9%": 3.291
+  }
+}
+
+function main() {
+  var la = document.forms["RegForm"]["LA"];
+  var cl_la = document.forms["RegForm"]["CL-LA"];
+  var indep = document.forms["RegForm"]["indep"];
+  var repeat = document.forms["RegForm"]["repeat"];
+  var normal = document.forms["RegForm"]["normal"];
+  //var log = document.forms["RegForm"]["log"];
+  var cl_ci_la = document.forms["RegForm"]["CL-CI-LA"];
+  //var cl_ci_bias = document.forms["RegForm"]["CL-CI-bias"]
+  var csv = document.forms["RegForm"]["csv"]
+
+  var alert_text = "Falta algun camp per omplir o  hi ha algun camp incorrecte.";
+
+  if (la.value == "" && !isNaN(la.value)) {
+      window.alert(alert_text);
+      la.focus();
+      return false;
+  }
+
+  if (indep.value == "NO" && repeat.value == "" && !isNaN(repeat.value)) {
+      window.alert(alert_text);
+      repeat.focus();
+      return false;
+  }
+
+  var input = csv.files[0];
+  var reader = new FileReader();
+
+  reader.onload = function (e) {
+    const csv_data = e.target.result;
+    var [arr_A, arr_B, mean_arr, diff_arr] = data_transformation(csv_data, normal.value);
+    var [bias, ci_bias, la_values, ci_la_top, ci_la_bottom, len, diff_sd] =
+      Bland_Altman(arr_A, arr_B, diff_arr, cl_la.value, cl_ci_la.value, indep.value, repeat.value)
+    //window.alert([diff_arr]);
+    graficar(mean_arr, diff_arr, bias, ci_bias, la_values, ci_la_top, ci_la_bottom);
+  };
+
+  reader.readAsText(input);
+
+  return true;
+}
+
+function mean_calc(arr){
+  var len = arr.length;
+
+  var sum = 0;
+  for(var i=0; i < len; i++){
+    sum += arr[i];
+  }
+
+  return sum/len;
+}
+
+function standard_deviation(arr){
+  var len = arr.length;
+  var mean = mean_calc(arr);
+
+  var sum = 0
+  for(var i=0; i < len; i++){
+    sum += Math.pow(arr[i] - mean, 2);
+  }
+
+  return Math.sqrt(sum / len);
+}
+
+function normalize(arr){
+  var x_max = Number.NEGATIVE_INFINITY;
+  var x_min = Number.POSITIVE_INFINITY;
+  for(var i=0; i < arr.length; i++){
+    var x = Number(arr[i]);
+    if(x > x_max){
+      x_max = x;
+    }
+    if(x < x_min){
+      x_min = x;
+    }
+  }
+
+  var new_arr = [];
+  for(var i=0; i < arr.length; i++){
+    var x = Number(arr[i]);
+    var new_x = 2*((x-x_min)/(x_max-x_min));
+    if(new_x == 0){
+      new_x = Math.pow(1, -10);
+    }
+    new_arr.push(new_x);
+  }
+
+  return new_arr;
+}
+
+function log_transformation(arr){
+  var norm_arr = normalize(arr);
+
+  var log_arr = [];
+  for(var i=0; i < norm_arr.length; i++){
+    log_arr.push(Math.log10(norm_arr[i]));
+  }
+
+  return log_arr;
+}
+
+function get_t_value(arr_len, conf){
+  if(arr_len <= 30){
+    var t_value = T_TABLE_CI[arr_len][conf];
+  } else if(arr_len <= 40){
+    var t_value = T_TABLE_CI[40][conf];
+  } else if(arr_len <= 60){
+    var t_value = T_TABLE_CI[60][conf];
+  } else if(arr_len <= 80){
+    var t_value = T_TABLE_CI[80][conf];
+  } else if(arr_len <= 100){
+    var t_value = T_TABLE_CI[100][conf];
+  } else if(arr_len <= 1000){
+    var t_value = T_TABLE_CI[1000][conf];
+  } else {
+    var t_value = T_TABLE_CI["z"][conf];
+  }
+
+  return t_value;
+}
+
+function BA_indep(bias, diff_sd, t_value_la, len){
+  var la_values = [
+    bias + (diff_sd*t_value_la),
+    bias - (diff_sd*t_value_la)
+  ];
+
+  var la_sd = Math.sqrt(Math.pow(diff_sd, 2)
+    *((1/len)+(Math.pow(t_value_la, 2)/(2*(len-1)))));
+
+  return [la_values, la_sd];
+}
+
+function BA_dep(bias, diff_sd, sd_A, sd_B, t_value_la, len, repeat){
+  var sd_modif = Math.sqrt(
+    Math.pow(diff_sd, 2)
+    + (1-(1/repeat))*Math.pow(sd_A, 2)
+    + (1-(1/repeat))*Math.pow(sd_B, 2)
+  );
+
+  var la_values = [
+    bias + (sd_modif*t_value_la),
+    bias - (sd_modif*t_value_la)
+  ];
+
+  var la_sd = Math.sqrt(
+    (Math.pow(sd_modif, 2)/len) + (Math.pow(t_value_la, 2)/(2*Math.pow(sd_modif, 2)))
+      *((Math.pow(diff_sd, 4)/(len-1))
+      + (((repeat-1)*Math.pow(sd_A, 4))/(len*Math.pow(repeat, 2)))
+      + (((repeat-1)*Math.pow(sd_B, 4))/(len*Math.pow(repeat, 2))))
+  );
+
+  return [la_values, la_sd];
+}
+
+function Bland_Altman(arr_A, arr_B, diff_arr, la_conf, ci_conf, indep, repeat){
+  var bias = mean_calc(diff_arr);
+  var diff_sd = standard_deviation(diff_arr);
+  var sd_A = standard_deviation(arr_A);
+  var sd_B = standard_deviation(arr_B);
+  var len = diff_arr.length;
+  var t_value_la = T_TABLE_CI["z"][la_conf];
+  var t_value_ci_la = get_t_value(len, ci_conf);
+
+  var bias_sd = diff_sd/Math.sqrt(len);
+
+  var ci_bias = [
+    bias + (bias_sd * t_value_ci_la),
+    bias - (bias_sd * t_value_ci_la)
+  ];
+
+  if(indep == "SI"){
+    var [la_values, la_sd] = BA_indep(bias, diff_sd, t_value_la, len);
+  }else{
+    var [la_values, la_sd] = BA_dep(bias, diff_sd, sd_A, sd_B, t_value_la, len, repeat);
+  }
+
+  var ci_la_top = [
+    la_values[0] + (la_sd * t_value_ci_la),
+    la_values[0] - (la_sd * t_value_ci_la)
+  ];
+
+  var ci_la_bottom = [
+    la_values[1] + (la_sd * t_value_ci_la),
+    la_values[1] - (la_sd * t_value_ci_la)
+  ];
+
+  return [bias, ci_bias, la_values, ci_la_top, ci_la_bottom, len, diff_sd]
+}
+
+function data_transformation(csv_data, normal){
+
+  var rows = csv_data.slice(csv_data.indexOf("\n") + 1).split("\n");
+
+  var [method_A, method_B, mean_AB, difference_AB] = [[], [], [], []];
+
+  for(var i=0; i<rows.length; i++){
+    var row = rows[i].split(",");
+    var row = [Number(row[0]), Number(row[1])];
+
+    method_A.push(row[0]);
+    method_B.push(row[1]);
+    mean_AB.push((row[0]+row[1])/2);
+    difference_AB.push(row[0]-row[1]);
+  }
+
+  if(normal == "NO"){
+    difference_AB = log_transformation(difference_AB);
+  }
+
+  return [method_A, method_B, mean_AB, difference_AB];
+}
+
+function graficar(mean_arr, diff_arr, bias, ci_bias, la_values, ci_la_top, ci_la_bottom){
+  var x_max = Number.NEGATIVE_INFINITY;
+  var x_min = Number.POSITIVE_INFINITY;
+
+  var y_max = Number.NEGATIVE_INFINITY;
+  var y_min = Number.POSITIVE_INFINITY;
+  
+  var scatter_data = [];
+
+  for(var i = 0; i < mean_arr.length; i++){
+    var x = Number(mean_arr[i]);
+    var y = Number(diff_arr[i]);
+    scatter_data.push({x: x, y: y});
+    if(x > x_max){
+      x_max = x;
+    }
+    if(x < x_min){
+      x_min = x;
+    }
+    if(y > y_max){
+      y_max = y;
+    }
+    if(y < y_min){
+      y_min = y;
+    }
+  }
+
+  var x_diff = x_max - x_min;
+  var x_max = x_max + x_diff*0.15;
+  var x_min = x_min - x_diff*0.15;
+
+  var y_diff = y_max - y_min;
+  var y_max = y_max + y_diff*0.05;
+  var y_min = y_min - y_diff*0.05;
+  
+  var data = {
+    datasets: [
+      //Set y max and min positions.
+      {
+        type: 'scatter',
+        data: [{
+          x: x_min,
+          y: y_min
+        }, {
+          x: x_max,
+          y: y_max
+        }],
+        pointRadius: 0
+      },
+
+      //Draw the 0 line.
+      {
+        type: 'line',
+        data: [{
+          x: x_min,
+          y: 0
+        }, {
+          x: x_max,
+          y: 0
+        }],
+        pointRadius: 0,
+        borderColor: 'rgb(0,0,0, 0.5)'
+      },
+
+      //Draw the points.
+      {
+        type: 'scatter',
+        data: scatter_data,
+        backgroundColor: 'rgb(155, 132, 232)'
+      },
+
+      //Draw the bias and its CI.
+      {
+        type: 'line',
+        data: [{
+          x: x_min,
+          y: bias
+        }, {
+          x: x_max,
+          y: bias
+        }],
+        pointRadius: 0,
+        borderColor: 'rgb(255, 99, 132)',
+        borderWidth: 4
+      },
+      {
+        type: 'line',
+        data: [{
+          x: x_min,
+          y: ci_bias[0]
+        }, {
+          x: x_max,
+          y: ci_bias[0]
+        }],
+        pointRadius: 0,
+        borderColor: 'rgb(255, 99, 132, 0.3)'
+      },
+      {
+        type: 'line',
+        data: [{
+          x: x_min,
+          y: ci_bias[1]
+        }, {
+          x: x_max,
+          y: ci_bias[1]
+        }],
+        pointRadius: 0,
+        borderColor: 'rgb(255, 99, 132, 0.3)'
+      },
+
+      //Draw top LA and its CI.
+      {
+        type: 'line',
+        data: [{
+          x: x_min,
+          y: la_values[0]
+        }, {
+          x: x_max,
+          y: la_values[0]
+        }],
+        pointRadius: 0,
+        borderColor: 'rgb(99, 132, 255)',
+        borderWidth: 4
+      },
+      {
+        type: 'line',
+        data: [{
+          x: x_min,
+          y: ci_la_top[0]
+        }, {
+          x: x_max,
+          y: ci_la_top[0]
+        }],
+        pointRadius: 0,
+        borderColor: 'rgb(99, 132, 255, 0.3)'
+      },
+      {
+        type: 'line',
+        data: [{
+          x: x_min,
+          y: ci_la_top[1]
+        }, {
+          x: x_max,
+          y: ci_la_top[1]
+        }],
+        pointRadius: 0,
+        borderColor: 'rgb(99, 132, 255, 0.3)'
+      },
+
+      //Draw bottom LA and its CL.
+      {
+        type: 'line',
+        data: [{
+          x: x_min,
+          y: la_values[1]
+        }, {
+          x: x_max,
+          y: la_values[1]
+        }],
+        pointRadius: 0,
+        borderColor: 'rgb(99, 132, 255)',
+        borderWidth: 4
+      },
+      {
+        type: 'line',
+        data: [{
+          x: x_min,
+          y: ci_la_bottom[0]
+        }, {
+          x: x_max,
+          y: ci_la_bottom[0]
+        }],
+        pointRadius: 0,
+        borderColor: 'rgb(99, 132, 255, 0.3)'
+      },
+      {
+        type: 'line',
+        data: [{
+          x: x_min,
+          y: ci_la_bottom[1]
+        }, {
+          x: x_max,
+          y: ci_la_bottom[1]
+        }],
+        pointRadius: 0,
+        borderColor: 'rgb(99, 132, 255, 0.3)'
+      },
+    ],
+  };
+
+  var config = {
+      type: 'scatter',
+      data: data,
+      options: {
+        scales: {
+          x: {
+            type: 'linear',
+            position: 'bottom'
+          }
+        },
+        plugins: {   
+          legend: {
+            display: false
+          },
+        }
+      }
+    };
+
+  var ctx = document.getElementById('myChart');
+  var myChart = new Chart(ctx, config);
+}
